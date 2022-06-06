182a202a2935d51c588df4196ca42fbeb1f89435
moonrailgun <moonrailgun@gmail.com>
2022-06-06 12:50:12 +0800

    refactor: migrate to v3

--- extension/background.js
+++ extension/background.js
@@ -1,54 +1,34 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
+function injectedFunction(extensionId) {
+  if(!window.becomeWaifu) {
+    window.becomeWaifu = true;
+    console.log('开始注入脚本');
+    var a = document.createElement('script');
+    a.setAttribute('src', `chrome-extension://${extensionId}/vendor/env.js`);
+    a.setAttribute('data-become-waifu', "1");
+    a.setAttribute('data-runtime-id', `${extensionId}`);
+    console.log(`chrome-extension://${extensionId}/vendor/env.js`)
+    document.body.appendChild(a);
+    var b = document.createElement('script');
+    b.setAttribute('src', `chrome-extension://${extensionId}/lib/become-waifu.js`);
+    document.body.appendChild(b);
+  } else {
+    console.log('不能重复注入');
+  }
+}
+
+chrome.action.onClicked.addListener(function (tab) {
   if(!tab.id) {
     console.log('No tab id, 跳过');
     return;
   }
 
   console.log('开始注入脚本');
-  chrome.tabs.executeScript(tab.id, {
-    // 通过这种方式提权
-    code: `
-    if(!window.becomeWaifu) {
-      window.becomeWaifu = true;
-      console.log('开始注入脚本');
-      var a = document.createElement('script');
-      a.setAttribute('src', "chrome-extension://${chrome.runtime.id}/vendor/env.js");
-      a.setAttribute('data-become-waifu', "1");
-      a.setAttribute('data-runtime-id', "${chrome.runtime.id}");
-      document.body.appendChild(a);
-      var b = document.createElement('script');
-      b.setAttribute('src', "chrome-extension://${chrome.runtime.id}/lib/become-waifu.js");
-      document.body.appendChild(b);
-    } else {
-      console.log('不能重复注入');
-    }
-    `,
+  chrome.scripting.executeScript({
+    target: {
+      tabId: tab.id,
+    },
+    world: 'MAIN',
+    func: injectedFunction,
+    args: [chrome.runtime.id]
   });
 });
-
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    // console.log(details);
-    if (details.url.endsWith('face_mesh_solution_packed_assets.data')) {
-      // console.log('redirect')
-      return {
-        redirectUrl:
-          'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/face_mesh_solution_packed_assets.data',
-      };
-    }
-  },
-  {
-    urls: ['<all_urls>'],
-    types: [
-      'main_frame',
-      'sub_frame',
-      'stylesheet',
-      'script',
-      'image',
-      'object',
-      'xmlhttprequest',
-      'other',
-    ],
-  },
-  ['blocking']
-);
